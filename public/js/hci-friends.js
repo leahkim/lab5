1799d04c74a45a253e4e5a8dcadd828aec631870
vagrant <vagrant@precise32.(none)>
2014-02-05 21:40:10 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$(".friendList").click(nameClicked);
 })
 
 /*
@@ -10,6 +11,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+}
+
+function nameClicked(e) {
+	e.preventDefault();
+	var name = $(this).find("h3").text();
+	console.log(name + " is clicked!");
+	var changedName = anagrammedName(name);
+	$(this).find("h3").text(changedName);
 }
 
 function anagrammedName(name) {
